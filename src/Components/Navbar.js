743f2462b0e403498e9d6ef9d47ccf614b85d682
shiv-pt <shivamp.is19@rvce.edu.in>
2020-07-30 17:06:26 +0530

    Update Navbar.js
    Icons added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import 'font-awesome/css/font-awesome.min.css';
 
 const Navbar = () => {
     return(
         <nav className = 'nav-wrapper grey darken-3'>
             <div className="container">
-                <a className="brand-logo " href='/'>Shopping List</a>
+                <a className="brand-logo " href='/'><span className="fa fa-lg fa-shopping-cart"> Shopping List</span></a>
                 <ul className="right">
-                <li><a href="/">List</a></li>
-                <li><a href='/stores'>Stores</a></li>
-                <li><a href='/dday'>D-Day</a></li>
+                <li><a href="/"><span className="fa fa-reorder"> List</span></a></li>
+                <li><a href='/stores'><span className="fa fa-home"> Stores</span></a></li>
+                <li><a href='/dday'><span className="fa fa-pencil"> D-Day</span></a></li>
                 </ul>
             </div>
         </nav> 
